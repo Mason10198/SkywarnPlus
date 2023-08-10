d713ca3493ed3631c4fe1638eee3c8a40ce02c0a
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2023-08-10 11:35:13 -0500

    v0.5.1 update

--- SkyDescribe.py
+++ SkyDescribe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-SkyDescribe.py v0.5.0 by Mason Nelson
+SkyDescribe.py v0.5.1 by Mason Nelson
 ==================================================
 Text to Speech conversion for Weather Descriptions
 
